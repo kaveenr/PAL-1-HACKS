a910c52f20ff2acfd0edd3107e1918b930085897
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2022-09-13 06:27:31 +0530

    some updates

--- kimio/kimio.s
+++ kimio/kimio.s
@@ -2,55 +2,75 @@
 
 ;	Reference To ROM Routines In The KIM-1
 	
-	SCANDS = $1f1f
-	GETKEY = $1f6a
+	SCANDS 	=	$1f1f
+	GETKEY 	= 	$1f6a
+	
+;	Reference to SCANDS values
+	dig12	=	$f9
+	dig34	=	$fa
+	dig56	=	$fb
 
-; Start program at free RAM.
+
+; 	Start program at free RAM.
 	.org $0200
 
-; Initialize SCANDS Values
-	lda #0
-	sta $f9
-	sta $fa
-	sta $fb
+init:
+	cld					; Clear Decimal
+	lda #$00			; Initialize SCANDS Values
+	sta dig12
+	sta dig34
+	sta dig56
 
-loop:	
+main_loop:
 	jsr SCANDS
 	jsr GETKEY
 
-	cmp #$15		; Return if no key pressed
-	beq loop
+	cmp #$15			; Return if no key pressed
+	beq main_loop
 
-	cmp #$10		; AD Key is pressed
+	cmp #$10			; AD Key is pressed
 	beq add_value
 	
-	cmp #$11		; DA Key is pressed
+	cmp #$11			; DA Key is pressed
 	beq sub_value
+	
+	cmp #$14			; PC Key is pressed
+	beq clear_value
 
-	sta $f9			; Valid Key Press
+	sta dig12			; Valid Key Press
 
-	inc $fd			; why do I have to do this?
-	bne loop		; Tell me why :') ????
+	inc $fd				; why do I have to do this?
+	bne main_loop		; Tell me why :') ????
 
-	jmp loop
+	jmp main_loop
 	
 
 add_value:
-	lda $fa
-	adc $f9
-	sta $fa
+	lda dig34
+	adc dig12
+	sta dig34
 	
-	inc $fd			; why do I have to do this?
-	bne loop		; Tell me why :') ????
+	inc $fd				; why do I have to do this?
+	bne main_loop		; Tell me why :') ????
 	
-	jmp loop
+	jmp main_loop
 	
 sub_value:
-	lda $fa
-	sbc $f9
-	sta $fa
+	lda dig34
+	sbc dig12
+	sta dig34
 	
-	inc $fd			; why do I have to do this?
-	bne loop		; Tell me why :') ????
+	inc $fd				; why do I have to do this?
+	bne main_loop		; Tell me why :') ????
 	
-	jmp loop
+	jmp main_loop
+	
+clear_value:
+	lda #0
+	sta dig34
+	sta dig56
+	
+	inc $fd				; why do I have to do this?
+	bne main_loop		; Tell me why :') ????
+	
+	jmp main_loop
