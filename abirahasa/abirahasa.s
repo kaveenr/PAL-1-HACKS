468e79e3936d6e093211111f92ee62b060638bed
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2022-09-18 22:18:41 +0530

    added more rooms

--- abirahasa/abirahasa.s
+++ abirahasa/abirahasa.s
@@ -370,7 +370,9 @@ s_err6:     .byte "Can't use a ",0
 game:       .byte (>@1),(<@1),(0  ),(0  ),(0  ),(0  ),(0  ),(0  ),  (<@t1),(>@t1)
 @1:         .byte (>@1),(<@3),(0  ),(0  ),(<@2),(0  ),('N'),('K'),  (<@t2),(>@t2), (<@e1),(>@e1)
 @2:         .byte (>@1),(0  ),(<@1),(0  ),(0  ),('K'),(0  ),(0  ),  (<@t3),(>@t3)
-@3:         .byte (0  ),(0  ),(0  ),(0  ),(0  ),(0  ),(0  ),(0  ),  (<@t4),(>@t4)
+@3:         .byte (>@3),(<@5),(<@4),(0  ),(0  ),(0  ),('N'),('L'),  (<@t4),(>@t4), (<@e2),(>@e2)
+@4:         .byte (>@3),(0  ),(0  ),(0  ),(<@3),('L'),(0  ),(0  ),  (<@t5),(>@t5)
+@5:         .byte (0  ),(0  ),(0  ),(0  ),(0  ),(0  ),(0  ),(0  ),  (<@t6),(>@t6)
 
 @t1:        .byte "You're at the side of an empty road, north of you is a foot path..."
             .byte CR,LF,"There is a sign that says 'Welcome To Abirahasa' next to the path.",0
@@ -382,7 +384,16 @@ game:       .byte (>@1),(<@1),(0  ),(0  ),(0  ),(0  ),(0  ),(0  ),  (<@t1),(>@t1
             .byte CR,LF,"At the back is a bench with broken car parts."
             .byte CR,LF,"You go closer to see a jar full of bolts... Shining in it is a key!",0
             
-@t4:        .byte "After opening the door with hesitation, you're in an untidy living room."
-            .byte CR,LF,"TODO says the ghost! Oooohohhohohoo!", 0
+@t4:        .byte "With hesitation the door creaks open to a dingy and dusty living room..."
+            .byte CR,LF,"It's much bigger space that anticipated, ahead is darkness."
+            .byte CR,LF,"There seems to be a kitchen area to the right", 0
+
+@t5:        .byte "You break the cobwebs and navigate to the kitchen, there is a book on the table"
+            .byte CR,LF,"the cover seems familiar to you. Next to it is a lamp.",0
+            
+@t6:        .byte "With the light of the lamp, you navigate to an open door way which leads down."
+            .byte CR,LF,"The floorboards creak as you take every step. You're shocked to see a lit candle"
+            .byte CR,LF,"on the table in the corner. No one seems to be here through!",0
             
 @e1:        .byte "Door is locked, you need to USE a key!",0
+@e2:        .byte "It's too dark to go foward!",0
